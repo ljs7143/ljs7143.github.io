004ed13b9de791141e1e8722c8805e50a6ad3f7b
ljs7143 <junseo27643779@gmail.com>
2023-04-15 22:54:42 +0900

    implemented clock&login

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,8 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello(){
-    console.log("say hello");
+function getClock(){
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
-
-setInterval(sayHello, 5000);
+getClock()   //이렇게 하면 바로 실행되어서 1초 후 시간을 표시하는 것을 막을 수 있음 
+setInterval(getClock, 1000);
