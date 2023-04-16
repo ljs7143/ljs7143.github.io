1b1964690566222c613c7091f3f87668eaf1daa4
ljs7143 <junseo27643779@gmail.com>
2023-04-16 22:32:28 +0900

    need to implement cancel button

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,25 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+
+
+
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value; //input의 현재 value를 새로운 변수에 복사
 
     toDoInput.value = "";    //submit버튼에 입력한 값이 지워지도록
+    paintToDo(newTodo);
 
 
 
