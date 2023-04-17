db6884dbcd684cf6dabeff3432c9acfa14089cc9
ljs7143 <junseo27643779@gmail.com>
2023-04-17 23:42:31 +0900

    Deleting To Do

--- js/todo.js
+++ js/todo.js
@@ -4,46 +4,59 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
-const toDos = [];
+let toDos = [];
 
-function saveToDos(){
-    localStorage.setItem(TODOS, JSON.stringify(toDos));
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
-function deleteToDo(event){
-    const li = event.target.parentElement;
-    li.remove();
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  console.log(li.id);
+  li.remove();
 }
-function paintToDo(newTodo){
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    span.innerText = newTodo;
-    const button = document.createElement("button");
-    button.innerText ="❌";
-    button.addEventListener("click", deleteToDo);
-    li.appendChild(span);
-    li.appendChild(button);
-    toDoList.appendChild(li);
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
 }
-function handleToDoSubmit(event){
-    event.preventDefault();
-    const newTodo = toDoInput.value; //input의 현재 value를 새로운 변수에 복사
-    toDoInput.value = "";    //submit버튼에 입력한 값이 지워지도록
-    toDos.push(newTodo); 
-    paintToDo(newTodo);
-    saveToDos();
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
 }
+
 toDoForm.addEventListener("submit", handleToDoSubmit);
-
-function sayHello(item){
-    console.log("this is the turn of", item);
-}
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
-if(saveToDos !==null){
-    const parsedToDos = JSON.parse(savedToDos);
-    console.log(parsedToDos);
-    parsedToDos.forEach((item) => console.log("this is the turn of", item));
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
+
+function sexyFilter(){
+
+    
 
 }
+
+
+
