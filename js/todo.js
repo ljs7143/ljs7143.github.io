d5f14f7657f9a27804a3871b913dbbf859e251cd
ljs7143 <junseo27643779@gmail.com>
2023-04-17 11:53:16 +0900

    Saving To Dos

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,11 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
 
 function deleteToDo(event){
     const li = event.target.parentElement;
@@ -9,7 +14,6 @@ function deleteToDo(event){
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-
     span.innerText = newTodo;
     const button = document.createElement("button");
     button.innerText ="❌";
@@ -17,13 +21,13 @@ function paintToDo(newTodo){
     li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
-
 }
-
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value; //input의 현재 value를 새로운 변수에 복사
     toDoInput.value = "";    //submit버튼에 입력한 값이 지워지도록
+    toDos.push(newTodo); 
     paintToDo(newTodo);
+    saveToDos();
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
