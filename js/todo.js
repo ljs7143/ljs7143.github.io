79ea9f2aae5ee4b5714dbebec8a7a20532055f04
ljs7143 <junseo27643779@gmail.com>
2023-04-17 11:01:17 +0900

    Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,15 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-
-
-
-
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
+
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText ="❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 
 }
@@ -18,11 +23,7 @@ function paintToDo(newTodo){
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value; //input의 현재 value를 새로운 변수에 복사
-
     toDoInput.value = "";    //submit버튼에 입력한 값이 지워지도록
     paintToDo(newTodo);
-
-
-
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
