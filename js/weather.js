ca88cac7993fdf2a1b185be42b999d494546f40f
ljs7143 <junseo27643779@gmail.com>
2023-04-18 19:25:16 +0900

    Add API key and ignore .env file

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,20 @@
-const API_KEY = "5cff7b3d84bddd5c32a2679e88c0c195";
+
 
 
 function onGeoOK(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     console.log("you live in", lat, lng);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
-    fetch(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json())
+    .then((data) => {
+        const weatherContainer = document.querySelector("#weather span:first-child");
+        const cityContainer = document.querySelector("#weather span:last-child");
+
+
+        cityContainer.innerText = data.name;
+        weatherContainer.innerText = data.weather[0].main;
+    });
 
 }
 function onGeoError(){
